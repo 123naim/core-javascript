bac49934fdbed005b5b03e754840862255694023
123naim <mdnayemiqbal886@gmail.com>
2024-01-14 10:01:53 +0600

    syntax runnign

---
+++ js-syntax/syntax.js
@@ -0,0 +1,23 @@
+// JavaScript syntax of the set of rules, how javascript programs are
+// How to create variables
+var x;
+var y;
+
+// How to use variables
+x = 5;
+y = 3;
+let z = x + y;
+
+
+// JavaScript Values
+// the javascript syntax defines two types of vlaue
+// fixed values and variables values
+// fixed value are called literals
+// variables value are called variable
+
+
+// JavaScript Literals
+// the two more important syntax rules for fixed values are: 
+// 1. Numbers are written with or without decimals
+// 2. String are text, written within double and single qoutes
+"double qoutes" & 'single qoutes'
