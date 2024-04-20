5f02b9975f30722b632a81892c22bd0d375dd0b8
123naim <mdnayemiqbal886@gmail.com>
2024-04-20 10:38:31 +0600

    new file added

---
+++ js-condition.js
@@ -0,0 +1,20 @@
+var a = 15;
+var b = 10;
+
+if (a > b) {
+    console.log('a er value boro')
+}
+if (a < b) {
+    console.log('b er value boro')
+}
+
+
+const person1 = 14;
+const person2 = 23;
+
+if (person1 > person2) {
+    console.log('person 1 boro')
+}
+else{
+    console.log('person 2 boro')
+}
