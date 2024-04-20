70e1d8e263c8b83ca7f6e3facbc0c00b7bd75bda
123naim <mdnayemiqbal886@gmail.com>
2024-04-20 10:29:12 +0600

    js new start

---
+++ Js Need to know/js-dataTypes.js
@@ -0,0 +1,33 @@
+// Primitive
+// Number
+// 10, 4.5, 12, 8
+// String
+// 'this is a txt' + 'this is also text'
+
+// Boolean
+// true false
+
+// undefined
+// null
+
+//  Object dataTypes
+// Array
+// object
+// function
+
+
+
+//  ******** NUMBER ********
+
+var n = 34;
+var f = 3.23;
+var nn = Number(43)
+console.log(nn)
+
+
+const myName = 'nayem';
+console.log(myName)
+
+
+
+
--- js-statement/statement.js
+++
@@ -1,7 +0,0 @@
-let x, y, z;
-x = 5;
-y = 3;
-z = x + y;
-console.log(z)
-
-// JavaScript statement compose of: vlaues, oparetors, expreesion, keyword and comments.
