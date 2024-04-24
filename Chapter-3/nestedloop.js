a155626516237a1c964a7dff5e4c9105aca1b356
123naim <mdnayemiqbal886@gmail.com>
2024-04-24 10:16:27 +0600

    nestedLoop

---
+++ Chapter-3/nestedloop.js
@@ -0,0 +1,33 @@
+// Nested Loop
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+
+let input = 5;
+for (let i = 1; i <= input; i++){
+    let result = '';
+    for (let j = 1; j <= i; j++){
+        result += j + ' ';
+    }
+}
+
+
+// challange
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+let starNumber = 10;
+
+for (let i = 1; i <= starNumber; i++){
+    let result = ' ';
+    for(let j = 1; j <= starNumber; j++){
+        result +='* ';
+    }
+    console.log(result)
+}
