7343f408edf4c66c2174ba4a8341c4bdd92a9ccf
123naim <mdnayemiqbal886@gmail.com>
2024-05-18 13:37:49 +0600

    added

--- Chapter-5/reverse.js
+++ Chapter-5/reverse.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 5, 6, 7];
 // Not a perfect way
 // for(let i = arr.length; i >= 0; i--){
-//     console.log(arr[i])
+//    console.log(arr[i])
 // }
 
 for (let i = 0; i < (arr.length / 2); i++){
