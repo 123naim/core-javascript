255bfa72deb86ec463d40788d580f2587a49bc08
123naim <mdnayemiqbal886@gmail.com>
2024-05-18 22:34:29 +0600

    Chapter 6 start

---
+++ Chapter-6/objectDeclear.js
@@ -0,0 +1,25 @@
+// Chapter six: Object is JavaScript 
+
+// const obj = {
+//     studentName: 'Nayem',
+// }
+// obj.studentId = 4001;
+// console.log(obj)
+
+// console.log(typeof obj)
+
+var point = {
+    x: 10,
+    y: 20
+}
+// point.y = 30;
+point.z = 30;
+console.log(point)
+
+const obj = Object()
+obj.a = 10;
+console.log(obj)
+
+const obj2 = new Object();
+obj2.b = 20;
+console.log(obj2)
