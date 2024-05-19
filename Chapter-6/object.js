ab9ea4a2b4e7a310fed349eac872699a348c1409
123naim <mdnayemiqbal886@gmail.com>
2024-05-19 10:18:17 +0600

    Access Object Properties

---
+++ Chapter-6/object.js
@@ -0,0 +1,14 @@
+const point = {
+    x: 10,
+    y: 20,
+    z: 15
+}
+
+console.log(point.z)
+console.log(point.x)
+console.log(point.x + point.y)
+
+console.log(point['y'])
+
+const show = 'x';
+console.log(point[show])
