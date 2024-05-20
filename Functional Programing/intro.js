467fd53296e991ef17ad86b25c9b477e71c91efa
123naim <mdnayemiqbal886@gmail.com>
2024-05-20 21:54:11 +0600

    Functional Progrming added

---
+++ Functional Programing/intro.js
@@ -0,0 +1,12 @@
+// Functional Programing with Javascript
+
+// What is functional Programing
+`Functional progrmaing is a programing paradigm-a style of building the structure and elements of computer programs-that treate computation as the evaluation of mathematical functions and avoids changing-state and mutable data-Wikipedia`
+
+// Three Main Terms
+    `Pure Functions`
+    `First class Functions`
+    `Higher Order Functions`
+
+
+    `******* Function Acts like a Value *******`
