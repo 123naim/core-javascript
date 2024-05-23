9ec1d757a4e849ce0557f7b81c1cc87e947caa95
123naim <mdnayemiqbal886@gmail.com>
2024-05-23 14:10:01 +0600

    added

--- Functional-Programing/find.js
+++ Functional-Programing/find.js
@@ -2,22 +2,51 @@ const arr = [3, 6, 32, 5, 2, 7, 4, 56, 73];
 const find = arr.find((value) => {
     return value === 3;
 })
-console.log(find)
+// console.log(find)
 
 const findIndex = arr.findIndex(value => value === 32)
-console.log(findIndex)
+// console.log(findIndex)
 
-function myfind(arr, cb){
-    for(let i = 0; i< arr.length; i++){
-        if(cb(arr[i])){
+function myfind(arr, cb) {
+    for (let i = 0; i < arr.length; i++) {
+        if (cb(arr[i])) {
             // return arr[i]
             return i
         }
     }
 }
 
-const result = myfind(arr, function(value) {
+const result2 = myfind(arr, function (value) {
     return value === 32;
 })
 
-console.log(result)
+// console.log(result2)
+
+
+// var grade = 'A';
+// var result = 0;
+// switch (grade) {
+//     case 'A':
+//         result += 10;
+//     case 'B':
+//         result += 9;
+//     case 'C':
+//         result += 8;
+//     default:
+//         result += 0;
+// }
+
+// console.log(result);
+
+
+
+// const obj1 = { property1: '10' };
+// const obj2 = Object.freeze(obj1);
+// obj2.property1 = '20';
+// console.log(obj2.property1);
+
+
+var i = 5; 
+for (; i < 5; i++) {
+console.log(i);
+}
