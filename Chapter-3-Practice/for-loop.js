9ab281a3cf796a5a98afbaa1bdaba912eb5a1e43
123naim <mdnayemiqbal886@gmail.com>
2024-05-24 23:44:05 +0600

    two example added

--- Chapter-3-Practice/for-loop.js
+++ Chapter-3-Practice/for-loop.js
@@ -55,9 +55,9 @@ console.log(min)
 
 
 // 6. for loop use kore choto theke boro porjonto sajate hbe
-for (let i = 1; i< arr.length; i++){
-    for (let j = 0; j < i; j++){
-        if(arr[i] > arr[j]){
+for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[i] > arr[j]) {
             let x = arr[i];
             arr[i] = arr[j];
             arr[j] = x;
@@ -70,9 +70,9 @@ console.log(arr)
 
 
 // 7. for loop use kore boro theke choto porjonto sajate hbe
-for(let i = 1; i< arr.length; i++){
-    for(let j = 0; j< i; j++){
-        if(arr[i] < arr[j]){
+for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+        if (arr[i] < arr[j]) {
             let x = arr[i];
             arr[i] = arr[j];
             arr[j] = x;
@@ -80,3 +80,31 @@ for(let i = 1; i< arr.length; i++){
     }
 }
 console.log(arr)
+
+
+// array use for chat gpt
+// উদাহরণ ১: অ্যারের সব সংখ্যার যোগফল নির্ণয়
+// let arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+// }
+// console.log("Total Sum:", sum);
+
+
+
+// উদাহরণ ২: অ্যারের সর্বোচ্চ এবং সর্বনিম্ন মান নির্ণয়
+// let arr = [34, 7, 23, 32, 5, 62];
+// let max = arr[0];
+// let min = arr[0];
+// for(let i = 1; i < arr.length; i++){
+//     if(arr[i] > max){
+//         max = arr[i];
+//     }
+//     if(arr[i] < min){
+//         min = arr[i];
+//     }
+// }
+// console.log("Max Value:", max);
+// console.log("Min Value:", min);
+
