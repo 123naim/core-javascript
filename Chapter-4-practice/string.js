0ed2e8ece4150720d0f4c5c9975517a28225eef0
123naim <mdnayemiqbal886@gmail.com>
2024-05-25 10:55:47 +0600

    string method practice

---
+++ Chapter-4-practice/string.js
@@ -0,0 +1,18 @@
+// type finding 
+// const str = 'String Literals'
+// const str2 = String('Something')
+// const str3 = new String('what does type')
+
+// console.log(typeof str)
+// console.log(typeof str2)
+// console.log(typeof str3)
+
+
+// convert number to string
+const n = 10;
+// not a right way
+const str = n + '';
+console.log(str)
+// right way
+const str2 = String(n)
+console.log(typeof str2)
