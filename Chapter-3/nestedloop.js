34cda9bf103de0bcfdec2b4fc6b1a21173f4956a
123naim <mdnayemiqbal886@gmail.com>
2024-06-04 11:30:40 +0600

    new array length implemetaion

--- Chapter-3/nestedloop.js
+++ Chapter-3/nestedloop.js
@@ -8,9 +8,9 @@
 // 1 2 3 4 5 6
 
 let input = 5;
-for (let i = 1; i <= input; i++){
+for (let i = input.length - 1; i <= 1; i--) {
     let result = '';
-    for (let j = 1; j <= i; j++){
+    for (let j = 1; j <= i; j++) {
         result += j + ' ';
     }
     console.log(result)
--- Chapter-4/length.js
+++ Chapter-4/length.js
@@ -10,17 +10,33 @@ let str2 = 'This is nayem'
 //     }
 // }
 
-function len(string){
+function len(string) {
     let length = 0;
-    while(true){
-        if(string.charAt(length) === ''){
+    while (true) {
+        if (string.charAt(length) === '') {
             break;
-        }else{
+        } else {
             length++;
         }
     }
     return length;
 }
 
+
 console.log(len(str))
-console.log(len(str2))
+console.log(len(str2));
+
+const arr = [1, 2, 3, 4, 5, 6];
+function len2(arr) {
+    let length2 = 0;
+    while (true) {
+        if (arr[length2] === undefined) {
+            break
+        } else {
+            length2++;
+        }
+    }
+    return length2;
+}
+
+console.log(len2(arr))
